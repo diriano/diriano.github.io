f69d349694e0f8a28fe36c1ffee6b74f14d98e61
Diego M Riano-Pachon <diriano@gmail.com>
2017-02-01 14:27:23 -0200

    update publication generation script

--- generatePublications.sh
+++ generatePublications.sh
@@ -2,11 +2,11 @@
 # Requires bibtex2html which in turn requires Objective Calm
 
 bibtex2html -d -r -t 'BCES Publications' -noabstract -nokeywords -noheader -nofooter dmrpPublications.bib 
-head -n 56 links.html |sed 's/<title>BCES - Links<\/title>/<title>BCES - Publications<\/title>/' > publications.html
+head -n 65 links.html |sed 's/<title>BCES - Links<\/title>/<title>BCES - Publications<\/title>/' > publications.html
 grep --after-context `wc -l dmrpPublications.html |sed -E 's/^ *//'|cut -f 1 -d' '` '<h1>BCES Publications</h1>' dmrpPublications.html |sed 's#</body>##'|sed 's#</html>##' >> publications.html
 tail -n 12 links.html >> publications.html
 sed -i bak 's/dmrpPublications\.html/publications.html/' dmrpPublications_bib.html
-head -n 56 links.html |sed 's/<title>BCES - Links<\/title>/<title>BCES - Publications bibtex<\/title>/' > dmrpPublications_bib2.html
+head -n 65 links.html |sed 's/<title>BCES - Links<\/title>/<title>BCES - Publications bibtex<\/title>/' > dmrpPublications_bib2.html
 grep --after-context `wc -l dmrpPublications_bib.html |sed -E 's/^ *//'|cut -f 1 -d' '` '<h1>dmrpPublications.bib</h1>' dmrpPublications_bib.html |sed 's#</body>##'|sed 's#</html>##' >> dmrpPublications_bib2.html
 tail -n 12 links.html >> dmrpPublications_bib2.html
 mv dmrpPublications_bib2.html dmrpPublications_bib.html
